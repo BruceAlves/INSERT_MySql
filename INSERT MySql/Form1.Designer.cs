8142d4b80ffaf9e652c258600c98e98cedee51b2
BruceAlves <brucewallace144@gmail.com>
2022-06-07 14:13:18 -0300

    Conexão MySql

---
+++ INSERT MySql/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace INSERT_MySql
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.butInserir = new System.Windows.Forms.Button();
+            this.butLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(22, 53);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(246, 23);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(291, 53);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(152, 23);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(22, 124);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(246, 23);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(291, 124);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(152, 23);
+            this.txtTelefone.TabIndex = 3;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "E-mail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(296, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Senha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefone";
+            // 
+            // butInserir
+            // 
+            this.butInserir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.butInserir.Location = new System.Drawing.Point(27, 174);
+            this.butInserir.Name = "butInserir";
+            this.butInserir.Size = new System.Drawing.Size(118, 33);
+            this.butInserir.TabIndex = 8;
+            this.butInserir.Text = "Inserir";
+            this.butInserir.UseVisualStyleBackColor = false;
+            this.butInserir.Click += new System.EventHandler(this.butInserir_Click);
+            // 
+            // butLimpar
+            // 
+            this.butLimpar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.butLimpar.Location = new System.Drawing.Point(290, 174);
+            this.butLimpar.Name = "butLimpar";
+            this.butLimpar.Size = new System.Drawing.Size(113, 33);
+            this.butLimpar.TabIndex = 9;
+            this.butLimpar.Text = "Limpar";
+            this.butLimpar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 232);
+            this.Controls.Add(this.butLimpar);
+            this.Controls.Add(this.butInserir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Cadastro INSERT MySQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button butInserir;
+        private System.Windows.Forms.Button butLimpar;
+    }
+}
---
+++ INSERT MySql/Form1.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INSERT_MySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butInserir_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string email = txtEmail.Text;
+            string senha = txtSenha.Text;
+            string telefone = txtTelefone.Text;
+            Guid codigo = Guid.NewGuid();
+
+            string conexao_MySql = "Server=127.0.0.1;Database=comfirmarLogin;Uid=root;Pwd=bruce@#2022;";
+
+            string comandoInsert = $@"insert into usuario (nome, email, senha, telefone, code, dataCadastro)
+            values ('{nome}', '{email}', '{senha}','{telefone}', '{codigo}', sysdate());";
+
+            MySqlConnection conexaoMySql = new MySqlConnection(conexao_MySql);
+
+            try
+            {
+                conexaoMySql.Open();
+
+                MySqlCommand executorComando = new MySqlCommand(comandoInsert, conexaoMySql);
+                executorComando.ExecuteNonQuery();
+
+                MessageBox.Show($"Usuario {nome} foi Inserido com sucesso", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro Banco de dados", $"Erro:\n{ex.Message}", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexaoMySql.Close();
+            }
+        }
+    }
+}
